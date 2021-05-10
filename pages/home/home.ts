bf859c963b5dd2a475e4bbecc7515f5739349484
SingedOpen <spookie.lp07@gmail.com>
2021-05-10 09:12:00 -0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { GodzillaPage } from '../filmes/Godzilla/godzilla';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  paginaGodzilla = Godzillapage;
 
   constructor(public navCtrl: NavController) {
 
